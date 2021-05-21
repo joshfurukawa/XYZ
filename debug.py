2741e918aaf86a853b1b14b2707b8d74ac0733e9
Sturz888 <rlsturza@student.rtc.edu>
2021-05-21 14:33:52 -0700

    Update debug.py
    Added input function so user can input values for a,b and c without editing code

--- debug.py
+++ debug.py
@@ -1,19 +1,18 @@
-
 def lone_sum(a, b, c):
-    
+      
     if a == b and a == c and b == c:
-        print (c)
         return c
     elif a == c:
-        print (b)
         return b
     elif b == c:
-        print (a)
         return a
     elif a >= b:
-        print (0)
         return 0
     else:
-        print (a + b + c)
-        return a+b+c
-lone_sum(10, 11, 12)
+        return a + b + c 
+
+a=int(input("Input A :"))
+b=int(input("Input B :"))
+c=int(input("Input C :"))
+
+print(lone_sum(a,b,c) )
