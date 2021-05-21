2ed51fd231101c0a79b59356eddc5c431d2aff19
Sturz888 <rlsturza@student.rtc.edu>
2021-05-21 14:13:07 -0700

    Changes to debug.py
    Moved a>=b down so that a == b a==c b ==c made sense. Added lone_sum(10, 11, 12) and code runs. Can change numbers as needed

--- debug.py
+++ debug.py
@@ -1,11 +1,19 @@
+
 def lone_sum(a, b, c):
-    if a >= b:
+    
+    if a == b and a == c and b == c:
+        print (c)
         return c
     elif a == c:
+        print (b)
         return b
     elif b == c:
+        print (a)
         return a
-    elif a == b and a == c and b == c:
+    elif a >= b:
+        print (0)
         return 0
     else:
+        print (a + b + c)
         return a+b+c
+lone_sum(10, 11, 12)
